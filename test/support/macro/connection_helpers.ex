99eb00eb1da49080081a894f42d15057b92e20e2
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2020-10-02 18:58:27 +0200

    Add tests

--- test/support/macro/connection_helpers.ex
+++ test/support/macro/connection_helpers.ex
@@ -13,11 +13,11 @@ defmodule Janus.Support.Macro do
   # macro generating test checking if event message handled by `Janus.Connection` module is passed to proper `Janus.Handler` callbacks
   # it used `FakeHandler.Payloads` mocked event messages and pass them through `Janus.Connection` which is supposed to pass them to proper `Janus.Handler` callbacks.
   # it uses `FakeHandler` callbacks to store last called callback in state, then it asserts that it was called for proper event type
-  defmacro test_callback(event) do
+  defmacro test_callback(event, index \\ 0) do
     fun = String.to_atom("handle_" <> Atom.to_string(event))
 
     quote do
-      test "#{inspect(unquote(fun))} callback" do
+      test "#{inspect(unquote(fun))} callback with payload nr #{unquote(index)}" do
         alias Janus.Connection
         alias Janus.MockTransport
         alias Janus.Support.FakeHandler
@@ -30,7 +30,7 @@ defmodule Janus.Support.Macro do
             handler_state: %{callback: nil}
           )
 
-        message = apply(Payloads, unquote(event), [])
+        message = apply(Payloads, unquote(event), [unquote(index)])
 
         {:noreply, new_state} = Connection.handle_info(message, state)
         state(handler_state: %{callback: callback}) = new_state
